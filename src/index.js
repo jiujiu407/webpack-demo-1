33d4a9d47c7547fb9bd86f582faa88db4fa68e4b
jiujiu <1503874780@qq.com>
2021-03-09 14:53:25 +0800

    webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import x from './x.js'
-console.log(x)
-console.log('不能怂')
+import png from './assets/1.png'
+const div = document.getElementById('app')
+div.innerHTML = `<img src="${png}" >`
+
+const button = document.createElement('button')
+button.innerHTML = '懒加载'
+button.onclick = ()=>{
+    const promise = import('./lazy')
+    promise.then((module)=>{
+        //函数等于模块的默认值
+        const fn = module.default
+        fn()
+    },()=>{})
+}
+div.appendChild(button)
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -11,7 +11,40 @@ module.exports = {
     new HtmlWebpackPlugin({
     title:'见鬼',
     template:'src/assets/index.html'
-  }),
+  })
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        //把文件变成文件路径
+        loader: ["file-loader"]
+      },
+      {
+        test: /\.styl$/i,
+        loader: ["style-loader","css-loader","stylus-loader"], // compiles Less to CSS
+      },
+          {
+            test: /\.less$/i,
+            loader: ["style-loader","css-loader","less-loader"], // compiles Less to CSS
+          },
+      {
+        test: /\.scss$/i,
+        use: [
+          //style-loader把js字符串转换为style标签
+          "style-loader",
+          //css-loader把css转换为js字符串
+          "css-loader",
+          {
+            // sass-loader把sass转换为css
+            loader:"sass-loader",
+            options:{
+              implementation:require('dart-sass')
+            }
+          }
+        ],
+      },
+    ],
+  },
 };
 
